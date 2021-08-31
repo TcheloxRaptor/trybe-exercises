24a6122ef607f0ba75b2b376906b0740a3187809
Marcelo Maurício <mmauricio.design@gmail.com>
2021-08-31 17:54:31 -0300

    exercise 2: create a post for a route /hello, wich return a massage with the name

--- modulo_3/bloco_26/dia_4/exercises/exercises-of-API/index.js
+++ modulo_3/bloco_26/dia_4/exercises/exercises-of-API/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const bodyPasser = require('body-parser');
 
 const app = express();
+app.use(bodyPasser.json());
 
 // 1 - Crie uma rota GET /ping
 
 app.get('/ping', function (_req, res) {
   res.status(200).json({ message: 'pong' });
 });
+
+// -----------------------------
+
+// 2 - Crie uma rota POST /hello
+
+app.post('/hello', function (req, res) {
+  const { name } = req.body;
+
+  res.status(201).json({ message: `Hello, ${name}!` })
+});
+
+// -----------------------------
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo a porta 3001 http://localhost:3001');
