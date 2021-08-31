b3f5e30336d4f38a2da1b657edaa9398b99ddc66
Marcelo Maurício <mmauricio.design@gmail.com>
2021-08-31 18:01:55 -0300

    exercise 3: check if the age of user is above 17 or not, and send a different message for the answer

--- modulo_3/bloco_26/dia_4/exercises/exercises-of-API/index.js
+++ modulo_3/bloco_26/dia_4/exercises/exercises-of-API/index.js
@@ -22,6 +22,18 @@ app.post('/hello', function (req, res) {
 
 // -----------------------------
 
+// 3 - Crie uma rota POST /greetings
+
+app.post('/greetings', function (req, res) {
+  const { name, age } = req.body;
+
+  if (age <= 17) return res.status(401).json({ message: "Unauthorized" });
+
+  res.status(200).json({ message: `Hello, ${name}!` })
+});
+
+// -----------------------------
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo a porta 3001 http://localhost:3001');
 });
