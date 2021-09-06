50c7e703c0d310aa6eff9dd4e116efda4e6ef860
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-06 12:47:09 -0300

    Intall nodemon in dev mode, and change getAll function in Author model, for now work with MongoDB

--- modulo_3/bloco_27/dia_1/pratice_exercises/connection-MongoDB/models/Author.js
+++ modulo_3/bloco_27/dia_1/pratice_exercises/connection-MongoDB/models/Author.js
@@ -31,10 +31,18 @@ const serialize = (authorData) => ({
 // Busca todos os autores do banco.
 
 const getAll = async () => {
-    const [authors] = await connection.execute(
-        'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
-    );
-    return authors.map(serialize).map(getNewAuthor);
+  return connection()
+      .then((db) => db.collection('authors').find().toArray())
+          .then((authors) =>
+              authors.map(({ _id, firstName, middleName, lastName }) =>
+              getNewAuthor({
+                  id: _id,
+                  firstName,
+                  middleName,
+                  lastName,
+              })
+          )
+      );
 };
 
 /*
