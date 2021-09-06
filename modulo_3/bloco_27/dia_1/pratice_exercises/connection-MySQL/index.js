5fc982fe978489c15503b4742e70636c7239ed01
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-06 10:08:11 -0300

    put the code about examples in the course

--- modulo_3/bloco_27/dia_1/pratice_exercises/connection-MySQL/index.js
+++ modulo_3/bloco_27/dia_1/pratice_exercises/connection-MySQL/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Book = require('./models/Book');
 const Author = require('./models/Author');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 // Books Routes
 
@@ -45,6 +48,18 @@ app.get('/authors/:id', async (req, res) => {
   if (!author) return res.status(404).json({ message: 'Not found' });
 
   res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+      return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! '});
 });
 
 const PORT = process.env.PORT || 3000;
