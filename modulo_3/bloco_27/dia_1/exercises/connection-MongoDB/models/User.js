18b5c55c6703cc8f7b6108b659f7d40ad22397be
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-07 10:10:34 -0300

    feat: create a function where validate the infos about user

--- modulo_3/bloco_27/dia_1/exercises/connection-MongoDB/models/User.js
+++ modulo_3/bloco_27/dia_1/exercises/connection-MongoDB/models/User.js
@@ -1,9 +1,22 @@
 const connection = require('./connection');
 
+const passwordIsValid = (password) => {
+  if (!password || typeof password === 'string' || password.length < 6) return false;
+
+  return true;
+};
+
+const isValid = (firstName, lastName, email, password) => {
+  if (!firstName || !lastName || !email || passwordIsValid(password) === false) {
+    return false;
+  }
+
+  return true;
+}
+
 const create = async (firstName, lastName, email, password)  =>
   await connection()
     .then((db) => db.collection('user').insertOne({ firstName, lastName, email, password }));
-
 
 module.exports = {
   create,
