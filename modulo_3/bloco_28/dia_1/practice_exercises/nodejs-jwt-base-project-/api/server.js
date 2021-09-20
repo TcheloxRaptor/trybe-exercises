aa850c6862495831b117dbd100904d97c0ab6a1b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-20 16:16:03 -0300

    Added the connection with validateJWT middleware to execute the authorization

--- modulo_3/bloco_28/dia_1/practice_exercises/nodejs-jwt-base-project-/api/server.js
+++ modulo_3/bloco_28/dia_1/practice_exercises/nodejs-jwt-base-project-/api/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
+/* Aqui, importamos nossa função que valida se o usuário está ou não autenticado */
+const validateJWT = require('./auth/validateJWT');
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -11,6 +14,8 @@ app.use(bodyParser.json());
 
 const apiRoutes = express.Router();
 
+/* E a usamos como middleware na nossa rota, colocando-a antes do nosso controller. */
+apiRoutes.get('/api/posts', validateJWT, routes.getPosts);
 apiRoutes.get('/api/posts', routes.getPosts);
 apiRoutes.post('/api/users', routes.createUsers);
 apiRoutes.post('/api/login', routes.login);
